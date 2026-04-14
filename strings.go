4f3ee119ce7b4268cd4fb5df03c2ddfc65a418ee
cyber-gentle <info.abdavid@gmail.com>
2026-04-14 10:10:56 +0100

    feat: implemetation of func capitalize

--- strings.go
+++ strings.go
@@ -1,5 +1,9 @@
 package gen2
 
+import (
+	"strings"
+)
+
 func UpperCase(s string) string {
 
 	char := []rune(s)
@@ -25,4 +29,34 @@ func LowerCase(s string) string {
 		}
 	}
 	return string(newChar)
+}
+
+func Capitalize(s string) string {
+	var newWord = make([]string, 0)
+
+	words := strings.Fields(s)
+	for _, word := range words {
+		char := []rune(word)
+		if char[0] >= 97 && char[0] <= 122 {
+			char[0] -= 32
+		}
+		word = string(char)
+		newWord = append(newWord, word)
+	}
+	return strings.Join(newWord, " ")
+}
+
+func CapitaliseWord(s string) string {
+	var newWord = make([]string, 0)
+
+	words := strings.Fields(s)
+	for i := 0; i < len(words); i++ {
+		char := []rune(words[i])
+		if char[0] >= 90 && char[0] <= 122 {
+			char[0] -= 32
+		}
+		newWord = append(newWord, string(char))
+	}
+
+	return strings.Join(newWord, " ")
 }
