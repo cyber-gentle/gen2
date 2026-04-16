82b4ea0f01deddf8862c5f3d2306a0d757bd043c
cyber-gentle <info.abdavid@gmail.com>
2026-04-16 16:48:29 +0100

    updated the loop for each function

--- strings.go
+++ strings.go
@@ -7,7 +7,7 @@ import (
 func UpperCase(s string) string {
 
 	char := []rune(s)
-	for i := 0; i < len(char); i++ {
+	for i := range char {
 		if char[i] >= 90 && char[i] <= 122 {
 			char[i] -= 32
 		}
@@ -17,18 +17,14 @@ func UpperCase(s string) string {
 
 func LowerCase(s string) string {
 
-	var newChar = make([]rune, 0)
-
-	allChar := []rune(s)
-	for _, c := range allChar {
-		if c >= 65 && c <= 90 {
-			c += 32
-			newChar = append(newChar, c)
-		} else {
-			newChar = append(newChar, c)
+	char := []rune(s)
+	for i := range char {
+		if char[i] >= 65 && char[i] <= 90 {
+			char[i] += 32
 		}
+
 	}
-	return string(newChar)
+	return string(char)
 }
 
 func Capitalize(s string) string {
@@ -50,7 +46,7 @@ func CapitaliseWord(s string) string {
 	var newWord = make([]string, 0)
 
 	words := strings.Fields(s)
-	for i := 0; i < len(words); i++ {
+	for i := range words {
 		char := []rune(words[i])
 		if char[0] >= 90 && char[0] <= 122 {
 			char[0] -= 32
