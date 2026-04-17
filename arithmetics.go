dac89cb63828d948500f93b816435a2aa7c0e421
abrdavid <info.abdavid@gmail.com>
2026-04-17 15:33:53 +0100

    feat: implementation of func Sum and func Differenece

--- arithmetics.go
+++ arithmetics.go
@@ -1,5 +1,19 @@
 package gen2
 
-func Sum() {
+func Sum(n ...int) int {
+	var total int
 
+	for i := range n {
+		total += n[i]
+	}
+	return total
+}
+
+func Difference(n ...int) int {
+	var total int = n[0]
+
+	for i := 1; i <= len(n)-1; i++ {
+		total -= n[i]
+	}
+	return total
 }
